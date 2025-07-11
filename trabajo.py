1ef2576828b1a039ca314c6ce859f0a055e804ef
TEstebanGQ <217262372+TEstebanGQ@users.noreply.github.com>
2025-07-11 10:30:33 -0500

    Ejercicio de python

---
+++ trabajo.py
@@ -0,0 +1,9 @@
+ladoA = 0.0
+ladoB = 0.0
+ladoC = 0.0
+perimetro = 0.0
+ladoA = float(input(f'Escribe el ladoA: '))
+ladoB = float(input(f'Escribe el ladoB: '))
+ladoC = float(input(f'Escribe el ladoC: '))
+perimetro = ladoA + ladoB + ladoC
+print(f'El perimetro del triangulo: {perimetro} ')
